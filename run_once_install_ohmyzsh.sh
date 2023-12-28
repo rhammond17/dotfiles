61e8428442c6d6b1fa87b5199722e8aac3d3456f
Randall Hammond <register@randallhammond.com>
2023-12-28 13:36:51 -0800

    Check for oh-my-zsh.sh file instead of directory

--- run_once_install_ohmyzsh.sh
+++ run_once_install_ohmyzsh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
+if [ ! -f "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]; then
 	echo "Getting ohmyz.sh"
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
 else
